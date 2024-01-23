441c8df98f9a2e3657c1df5cf99ac14686d988e3
khushi-gunnu17 <khushiastrogeek@gmail.com>
2024-01-23 14:36:09 +0530

    Project

--- src/middlewares/file-upload.middleware.js
+++ src/middlewares/file-upload.middleware.js
@@ -2,7 +2,7 @@
 import multer from "multer";
 
 const storageConfig = multer.diskStorage({
-    destination : (rea, file, cb) => {
+    destination : (req, file, cb) => {
         cb(null, "public/images/")
     },
     filename : (req, file, cb) => {
